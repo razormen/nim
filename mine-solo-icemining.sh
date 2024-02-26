578527dc3c7d62c2fe9a0b1a72b03ea784304506
razormen <anggaoktavian90@gmail.com>
2024-02-26 15:42:01 +0700

    INSERT DATA

---
+++ mine-solo-icemining.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#!/bin/bash
+
+./noncerpro --address='NQ52 7TL5 RA6B SSAS FULC P3SR D88B X2CK 0VTX' --threads=4 --server=nimiq.icemining.ca --port=2053 -x='m=solo' 
---
+++ mine.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./noncerpro --address='NQ230G33JGEQSMUF3MG8VRYKARN3DJJCY6KU' --threads=2 --server=nimiq.icemining.ca --port=2053
